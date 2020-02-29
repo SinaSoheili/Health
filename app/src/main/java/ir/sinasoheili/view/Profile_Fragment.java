3e1c19a2b67b83a986a3a21a907f9efa6ac20de9
SINA <sinasoheili79@gmail.com>
2020-02-29 17:06:13 +0330

    set id to button setting in profile page and set temp dialog to onclick of theme

--- app/src/main/java/ir/sinasoheili/view/Profile_Fragment.java
+++ app/src/main/java/ir/sinasoheili/view/Profile_Fragment.java
@@ -1,9 +1,12 @@
 package ir.sinasoheili.view;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,7 +18,7 @@ import java.text.DecimalFormat;
 import presenter.Profile_Page_contract;
 import presenter.Profile_Page_presenter;
 
-public class Profile_Fragment extends Fragment implements Profile_Page_contract.Profile_Page_contract_view
+public class Profile_Fragment extends Fragment implements Profile_Page_contract.Profile_Page_contract_view, View.OnClickListener
 {
     private Profile_Page_contract.Profile_Page_contract_presenter presenter;
 
@@ -25,6 +28,11 @@ public class Profile_Fragment extends Fragment implements Profile_Page_contract.
     private TextView tv_age;
     private TextView tv_height;
     private TextView tv_weight;
+
+    private Button btn_change_font;
+    private Button btn_change_theme;
+    private Button btn_bug_report;
+    private Button btn_contact_us;
 
     @Nullable
     @Override
@@ -50,6 +58,18 @@ public class Profile_Fragment extends Fragment implements Profile_Page_contract.
         tv_age = root_view.findViewById(R.id.profile_tv_age);
         tv_height = root_view.findViewById(R.id.profile_tv_height);
         tv_weight = root_view.findViewById(R.id.profile_tv_weight);
+
+        btn_change_font = root_view.findViewById(R.id.profile_btn_change_font);
+        btn_change_font.setOnClickListener(this);
+
+        btn_change_theme = root_view.findViewById(R.id.profile_btn_change_theme);
+        btn_change_theme.setOnClickListener(this);
+
+        btn_bug_report = root_view.findViewById(R.id.profile_btn_bug_reporting);
+        btn_bug_report.setOnClickListener(this);
+
+        btn_contact_us = root_view.findViewById(R.id.profile_btn_contact_us);
+        btn_contact_us.setOnClickListener(this);
     }
 
     @Override
@@ -102,6 +122,42 @@ public class Profile_Fragment extends Fragment implements Profile_Page_contract.
         else
         {
             tv_weight.setText("وزن : ");
+        }
+    }
+
+    private void temp_dialog()
+    {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(root_view.getContext());
+        dialog.setMessage("این قسمت با عنوان good first issues به جهت سادگی برای مشارکت با دیگر برنامه نویس ها در github  و gitlab  قرار گرفته است");
+        dialog.setNegativeButton("باشه", new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        if(v.equals(btn_change_font))
+        {
+            temp_dialog();
+        }
+        else if(v.equals(btn_change_theme))
+        {
+            temp_dialog();
+        }
+        else if(v.equals(btn_bug_report))
+        {
+            temp_dialog();
+        }
+        else if(v.equals(btn_contact_us))
+        {
+            temp_dialog();
         }
     }
 }
